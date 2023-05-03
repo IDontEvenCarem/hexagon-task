93e594abd1cc7629a5158850503cdd9131d0213a
Kajetan Owczarek <flash3ch@gmail.com>
2023-05-03 15:51:45 +0200

    Conditional rendering progress, basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import DishForm from './DishForm'
 
 function App() {
   const dishSubmitHandler = data => {
-
+    alert(JSON.stringify(data, undefined, 2))
   }
 
   return (
--- src/DishForm.jsx
+++ src/DishForm.jsx
@@ -1,12 +1,14 @@
 import { formValueSelector } from 'redux-form'
 import {Field, reduxForm} from 'redux-form'
 import {useSelector} from 'react-redux'
+import './DishForm.css'
 
 function DishFieldText ({input, label, meta: {touched, error}}) {
     return (
         <div className='dishform__field'>
             <label className='dishform__label'>{label}</label>
             <input {...input}></input>
+            {touched && error && (<div className='dishform__error'>{error}</div>)}
         </div>
     )
 }
@@ -16,6 +18,7 @@ function DishFieldPreptime ({input, label, meta: {touched, error}}) {
         <div className='dishform__field'>
             <label className='dishform__label'>{label}</label>
             <input {...input} placeholder='00:00:00'></input>
+            {touched && error && (<div className='dishform__error'>{error}</div>)}
         </div>
     )
 }
@@ -45,15 +48,40 @@ function DishForm (props) {
 
     return (
         <form onSubmit={handleSubmit} className='dishform'>
-            <Field name="name" label="Name" component={DishFieldText}></Field>
+            <Field name="name" label="Name" placeholder='Suflet Mignion' component={DishFieldText}></Field>
             <Field name="preparation_time" label="Preparation Time" component={DishFieldPreptime}></Field>
             <Field name="type" label="Dish type" options={typeOptions} component={DishFieldOption}></Field>
+            {type === 'pizza' && (
+                <>
+                    <Field name='no_of_slices' label="Number of slices" component={DishFieldText}></Field>
+                    <Field name='diameter' label="Diameter" component={DishFieldText}></Field>
+                </>
+            )}
+            <input type="submit" value="Save" className='dishform__button'></input>
         </form>
     )
 }
 
 const validate = data => {
-
+    const errors = {}
+    if (!data.name) {
+        errors.name = "Name is required"
+    }
+    if (!data.preparation_time) {
+        errors.preparation_time = "Preparation time is required"
+    }
+    if (!data.type) {
+        errors.type = "You must select a type for the dish"
+    }
+    if (data.type && data.type === 'pizza') {
+        if (!data.no_of_slices) {
+            errors.no_of_slices = "Number of slices is required"
+        }
+        if (!data.diameter) {
+            errors.diameter = "Diameter is required"
+        }
+    }
+    return errors
 }
 
 export default reduxForm({
