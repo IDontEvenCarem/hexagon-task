1e4005f5442ed240bbfa3bafbc5c96c86e180b48
Kajetan Owczarek <flash3ch@gmail.com>
2023-05-03 18:25:20 +0200

    Prettiering the rest of the code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './App.css'
 import DishForm from './DishForm'
 
 function App() {
-  const dishSubmitHandler = data => {
-    alert(JSON.stringify(data, undefined, 2))
-  }
+    const dishSubmitHandler = (data) => {
+        alert(JSON.stringify(data, undefined, 2))
+    }
 
-  return (
-    <div className="center-content">
-      <DishForm onSubmit={dishSubmitHandler}></DishForm>
-    </div>
-  )
+    return (
+        <div className="center-content">
+            <DishForm onSubmit={dishSubmitHandler}></DishForm>
+        </div>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
-import {reducer as formReducer} from 'redux-form'
+import { reducer as formReducer } from 'redux-form'
 
 // This store is quite barebones, as we just dont have any real global state, other than the redux-form library internals
 const store = configureStore({
-  reducer: {
-    form: formReducer
-  }
+    reducer: {
+        form: formReducer,
+    },
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // redux-form is quite old, deprecated, and breaks strict mode
-  // <React.StrictMode>
+    // redux-form is quite old, deprecated, and breaks strict mode
+    // <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <App />
     </Provider>
-  // </React.StrictMode>,
+    // </React.StrictMode>,
 )
