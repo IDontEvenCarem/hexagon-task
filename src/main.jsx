002f895edd097c75fc8c8acbd8e30a38c87ee923
Kajetan Owczarek <flash3ch@gmail.com>
2023-05-03 11:25:10 +0200

    Added needed libraries

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import {reducer as formReducer} from 'redux-form'
+
+// This store is quite barebones, as we just dont have any real global state, other than the redux-form library internals
+const store = configureStore({
+  reducer: {
+    form: formReducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // redux-form is quite old, deprecated, and breaks strict mode
+  // <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  // </React.StrictMode>,
 )
